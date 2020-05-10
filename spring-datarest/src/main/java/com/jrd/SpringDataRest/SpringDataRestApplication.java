bca1a15f8a230694530f4d20eaa7e906b5c12b13
Jr Domingos <domingoshot@hotmail.com>
2020-05-10 12:26:50 -0300

    Update SpringDataRestApplication.java

--- spring-datarest/src/main/java/com/jrd/SpringDataRest/SpringDataRestApplication.java
+++ spring-datarest/src/main/java/com/jrd/SpringDataRest/SpringDataRestApplication.java
@@ -1,5 +1,7 @@
 package com.jrd.SpringDataRest;
 
+                            /* ***********  @Author : JrD - Junior Domingos *********** */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

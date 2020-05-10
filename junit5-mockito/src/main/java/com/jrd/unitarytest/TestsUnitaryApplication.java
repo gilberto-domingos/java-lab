3324f5a86271a292282566d28f730a2839a7781c
Jr Domingos <domingoshot@hotmail.com>
2020-05-10 12:23:27 -0300

    Update TestsUnitaryApplication.java

--- junit5-mockito/src/main/java/com/jrd/unitarytest/TestsUnitaryApplication.java
+++ junit5-mockito/src/main/java/com/jrd/unitarytest/TestsUnitaryApplication.java
@@ -1,5 +1,7 @@
 package com.jrd.unitarytest;
 
+                                /* ***********  @Author : JrD - Junior Domingos *********** */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

1028abc3b9affd55c9f9e4c3825fb043c5b12939
Your Name <domingoshot@hotmail.com>
2021-07-30 19:41:41 -0300

    create rh-payroll project

--- workwers-microservices/rh-worker/src/main/java/com/jrd/rhworker/controller/WokerController.java
+++ workwers-microservices/rh-worker/src/main/java/com/jrd/rhworker/controller/WokerController.java
@@ -2,8 +2,6 @@ package com.jrd.rhworker.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 

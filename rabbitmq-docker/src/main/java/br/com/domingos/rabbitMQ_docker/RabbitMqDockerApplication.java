7813e7b32e9234b329a8585a1bd37b943289b173
jr <yapr7777@gmail.com>
2023-09-17 11:22:57 -0300

    create project

---
+++ rabbitmq-docker/src/main/java/br/com/domingos/rabbitMQ_docker/RabbitMqDockerApplication.java
@@ -0,0 +1,13 @@
+package br.com.domingos.rabbitMQ_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqDockerApplication.class, args);
+	}
+
+}
---
+++ rabbitmq-docker/src/test/java/br/com/domingos/rabbitMQ_docker/RabbitMqDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domingos.rabbitMQ_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

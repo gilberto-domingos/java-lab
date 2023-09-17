2e174db36dc405e35c109e0a2e6f64dba84c4e70
jr <yapr7777@gmail.com>
2023-09-17 20:58:50 -0300

    change to lombok dependency

--- rabbitmq-docker/src/main/java/br/com/domingos/rabbitMQ_docker/consumer/UserConsumer.java
+++ rabbitmq-docker/src/main/java/br/com/domingos/rabbitMQ_docker/consumer/UserConsumer.java
@@ -2,8 +2,7 @@ package br.com.domingos.rabbitMQ_docker.consumer;
 
 import br.com.domingos.rabbitMQ_docker.entity.User;
 import br.com.domingos.rabbitMQ_docker.messengerConfig.UserMessageConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -11,10 +10,9 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 public class UserConsumer {
-    private static final Logger log = LoggerFactory.getLogger(UserConsumer.class);
     @RabbitListener(bindings = @QueueBinding(value = @Queue(UserMessageConfig.NAME_QUEUE),
             exchange = @Exchange(name = UserMessageConfig.NAME_EXCHANGE),
             key = UserMessageConfig.ROUTING_KEY))
--- rabbitmq-docker/src/main/java/br/com/domingos/rabbitMQ_docker/entity/User.java
+++ rabbitmq-docker/src/main/java/br/com/domingos/rabbitMQ_docker/entity/User.java
@@ -1,57 +1,16 @@
 package br.com.domingos.rabbitMQ_docker.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonTypeId;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@EntityScan
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
-    private String fone;
+    private String phone;
     private String email;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public User() {
-        // Construtor padrão vazio
-    }
-
-    public User(String name, String fone, String email) {
-        this.name = name;
-        this.fone = fone;
-        this.email = email;
-    }
-
-
 }
 
 

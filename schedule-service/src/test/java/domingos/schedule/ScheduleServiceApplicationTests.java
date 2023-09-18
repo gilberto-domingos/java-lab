d14746002e2d828cae86da434a99dee34451e94b
jr <yapr7777@gmail.com>
2023-09-18 18:55:24 -0300

    create shedule_service

---
+++ schedule-service/src/test/java/domingos/schedule/ScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package domingos.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

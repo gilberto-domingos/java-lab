d5fcbc1a1081e5f66a46a20b81c63c0a3d7d428b
jrdomingos <domingoshot@hotmail.com>
2023-10-04 22:29:55 -0300

    Scaffolding Microservice dependencies

--- microserv-spring-cloud-mensageria-docker/README.md
+++ microserv-spring-cloud-mensageria-docker/README.md
@@ -1,1 +1,7 @@
 # ms-spring-cloud-mensageria-docker
+
+Cartão de Crédito - Microserviços, Spring Cloud, Mensageria RabbitMQ e Docker 
+
+- Eureka Server
+- Scaffolding Microservice, dependêncies
+
---
+++ microserv-spring-cloud-mensageria-docker/ms-client/src/test/java/br/com/domingos/msclient/MsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.domingos.msclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

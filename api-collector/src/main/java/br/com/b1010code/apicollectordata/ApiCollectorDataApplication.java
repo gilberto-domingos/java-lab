274374fbb11bce1fe3710b1c375ea5292fb3ac79
jrdomingos <domingoshot@hotmail.com>
2023-10-11 10:29:59 -0300

    create project

---
+++ api-collector/src/main/java/br/com/b1010code/apicollectordata/ApiCollectorDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.b1010code.apicollectordata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCollectorDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCollectorDataApplication.class, args);
+	}
+
+}

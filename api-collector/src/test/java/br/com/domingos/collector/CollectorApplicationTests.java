5f8c58f8edfaf9bad752b57caf4562d2d43ad734
jrdomingos <domingoshot@hotmail.com>
2023-10-13 20:22:32 -0300

    create jwt security

--- api-collector/src/test/java/br/com/domingos/collector/CollectorApplicationTests.java
+++ api-collector/src/test/java/br/com/domingos/collector/CollectorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JwtOauth2ApplicationTests {
+class CollectorApplicationTests {
 
 	@Test
 	void contextLoads() {
